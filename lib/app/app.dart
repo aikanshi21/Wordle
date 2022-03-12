932a0c8f3a23548d178be498bd21273948c25190
aikanshi21 <aikanshi21@gmail.com>
2022-03-12 22:11:52 +0530

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:wordle/wordle/views/wordle_screen.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "WORDLE",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
+      home: const WordleScreen(),
+    );
+  }
+}
